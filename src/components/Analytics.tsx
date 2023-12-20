fdef88fb48ba1d432c93aa03a0dfd1b6afd15971
mtellami <moadtell@gmail.com>
2023-12-20 20:27:36 +0100

    data virtualization, ChartJs

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,6 +1,6 @@
 function Analytics() {
 	return (
-		<section className="p-4 text-3xl font-bold">
+		<section className="p-4 text-3xl font-bold w-40">
 			<h1 className="capitalize">analytics</h1>
 			<div></div>
 		</section>
